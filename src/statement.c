5bda6304399c4496528c0b73f05fcd8d407a3a35
Kevin Deisz <kevin.deisz@gmail.com>
2016-07-15 15:34:02 -0400

    returning converted string

--- src/statement.c
+++ src/statement.c
@@ -8,6 +8,14 @@ statement_t* stmt_build(char *macro, node_t *actions, node_t *resources) {
   return stmt;
 }
 
+static char* ll_to_str(node_t *head) {
+  return "converted";
+}
+
 void stmt_to_json(statement_t *stmt) {
-  printf("{\n  \"Effect\": \"%s\",\n  \"Action\": %s,\n  \"Resource\":  %s\n}\n", stmt->macro, "alpha", "beta");
+  printf("{\n  \"Effect\": \"%s\",\n  \"Action\": %s,\n  \"Resource\":  %s\n}\n",
+    stmt->macro,
+    ll_to_str(stmt->actions),
+    ll_to_str(stmt->resources)
+  );
 }
