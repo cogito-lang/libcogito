51830f36a06eadf7a83a732f4f4738e8f454df16
Matt Dziuban <mrdziuban@gmail.com>
2016-07-19 09:05:50 -0400

    Update statement to use the json-c library.

--- src/statement.c
+++ src/statement.c
@@ -1,10 +1,5 @@
 #include "statement.h"
 
-// The size of the JSON string converted from the linked list
-static int converted_size(node_t *head) {
-  return 4 * ll_size(head) + ll_val_size_sum(head) + 1;
-}
-
 // Capitalize the macro string
 static void format_macro(char *macro) {
   int idx;
@@ -15,30 +10,32 @@ static void format_macro(char *macro) {
   }
 }
 
-// Convert the given linked list to a JSON-like array string
-static void ll_to_str(node_t *head, char *converted, int size) {
-  int location = 1;
-  converted[0] = '[';
+// Add the statement macro to the JSON object
+static void add_statement_macro(char *macro, json_object *json) {
+  format_macro(macro);
+  json_object_object_add(json, "Effect", json_object_new_string(macro));
+}
 
+// Add elements to a JSON array
+static void add_elements_to_array(node_t *head, json_object *array) {
   node_t *ptr = head;
-  while(ptr != NULL) {
-    converted[location++] = '"';
-
-    strncpy(converted + location, ptr->val, strlen(ptr->val));
-    location += strlen(ptr->val);
-
-    if (ptr->next == NULL) {
-      converted[location] = '"';
-    } else {
-      strncpy(converted + location, "\", ", 3);
-      location += 3;
-    }
-
+  while (ptr != NULL) {
+    json_object_array_add(array, json_object_new_string(ptr->val));
     ptr = ptr->next;
   }
+}
 
-  converted[size - 2] = ']';
-  converted[size - 1] = '\0';
+// Add a list of statement elements to the JSON object
+static void add_statement_elements(node_t *head, json_object *json, char *json_key) {
+  json_object *elements = json_object_new_array();
+  add_elements_to_array(head, elements);
+  json_object_object_add(json, json_key, elements);
+}
+
+static void cleanup_json(json_object* json) {
+  json_object_object_del(json, "Effect");
+  json_object_object_del(json, "Action");
+  json_object_object_del(json, "Resource");
 }
 
 // Build a statement object from the given macro, actions, and resources
@@ -52,15 +49,11 @@ statement_t* stmt_build(char *macro, node_t *actions, node_t *resources) {
 
 // Converts a given statement object to a JSON string
 void stmt_to_json(statement_t *stmt) {
-  int actions_size = converted_size(stmt->actions);
-  int resources_size = converted_size(stmt->resources);
-
-  char actions[actions_size];
-  char resources[resources_size];
-
-  ll_to_str(stmt->actions, actions, actions_size);
-  ll_to_str(stmt->resources, resources, resources_size);
-  format_macro(stmt->macro);
-
-  printf("{\n  \"Effect\": \"%s\",\n  \"Action\": %s,\n  \"Resource\": %s\n}\n", stmt->macro, actions, resources);
+  json_object *json = json_object_new_object();
+  add_statement_macro(stmt->macro, json);
+  add_statement_elements(stmt->actions, json, "Action");
+  add_statement_elements(stmt->resources, json, "Resource");
+  printf("%s\n", json_object_to_json_string_ext(json, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY));
+  // Free the memory allocated for the JSON object
+  cleanup_json(json);
 }
--- src/statement.h
+++ src/statement.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <json-c/json.h>
 #include "linked-list.h"
 
 typedef struct statement {
