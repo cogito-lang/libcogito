4651254d251ad86aed9e52f8f85158f3f5ecc03d
Kevin Deisz <kevin.deisz@gmail.com>
2016-08-19 16:10:34 -0400

    Create libcogito.a

---
+++ src/cogito.h
@@ -0,0 +1,10 @@
+#ifndef COGITO
+#define COGITO
+
+#include "cogito/linked-list.h"
+#include "cogito/statement.h"
+#include "cogito/parser.h"
+
+char* cg_parse(char *input);
+
+#endif
---
+++ test-files/libtest.c
@@ -0,0 +1,6 @@
+#include <cogito.h>
+
+int main() {
+  printf("%s\n", cg_parse("allow a on b;"));
+  return 0;
+}
