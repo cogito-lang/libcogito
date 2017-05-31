3094fffbe59eff26d23a0d39402bd1c778818056
Kevin Deisz <kevin.deisz@gmail.com>
2017-05-31 17:33:07 -0400

    Even more documentation

--- src/buffer.h
+++ src/buffer.h
@@ -1,18 +1,34 @@
-#ifndef COGITO_BUF
-#define COGITO_BUF
+#ifndef COGITO_BUFFER
+#define COGITO_BUFFER
 
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-typedef struct cg_buf {
+/**
+ * A struct representing an expandable string. Contains the current length of
+ * the string, the current capacity of the string, and a pointer to the string
+ * itself.
+ */
+typedef struct {
   size_t length;
   size_t capacity;
   char *content;
 } cg_buf_t;
 
+/**
+ * Initialize and return a new cg_buf_t struct.
+ */
 cg_buf_t* cg_buf_build(void);
+
+/**
+ * Copy a given string onto the given buffer, expanding if necessary.
+ */
 int cg_buf_append(cg_buf_t *buffer, const char *str);
+
+/**
+ * Free the allocated buffer and its associated string.
+ */
 void cg_buf_free(cg_buf_t *buffer);
 
 #endif
--- src/cogito.h
+++ src/cogito.h
@@ -6,7 +6,14 @@
 #include "cogito/statement.h"
 #include "cogito/parser.h"
 
+/**
+ * Convert the given input string to JSON and copy it onto the given buffer.
+ */
 int cg_to_json(cg_buf_t *buffer, char *input);
+
+/**
+ * Convert the given input string to IAM and copy it onto the given buffer.
+ */
 int cg_to_iam(cg_buf_t *buffer, char *input);
 
 #endif
